273cf90d5984e5f7204e061a89a61d7e94d6f748
nikolas <panfuming@Nikolas-Mbp.local>
2019-06-06 14:40:25 +0800

    Fix GetRemoteIP in multi-proxy

--- helpers/ip.go
+++ helpers/ip.go
@@ -3,14 +3,16 @@ package helpers
 import (
 	"net"
 	"net/http"
+	"strings"
 )
 
 // GetRemoteIP 返回远程客户端IP
 func GetRemoteIP(req *http.Request) string {
 	remoteAddr := req.RemoteAddr
-	if ip := req.Header.Get("X-Real-IP"); ip != "" {
-		remoteAddr = ip
-	} else if ip = req.Header.Get("X-Forwarded-For"); ip != "" {
+	if ip := req.Header.Get("X-Forwarded-For"); ip != "" {
+		// 多层代理情况的处理
+		remoteAddr = strings.Split(ip, ",")[0]
+	} else if ip = req.Header.Get("X-Real-IP"); ip != "" {
 		remoteAddr = ip
 	} else {
 		remoteAddr, _, _ = net.SplitHostPort(remoteAddr)
